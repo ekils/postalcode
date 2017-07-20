39d61b14f829b1e80ced9b224094433269818806
dannyliu <dannyliu@ixensor.com>
2017-07-20 17:10:33 +0800

    9th commit

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'postalcode',
-    #packages = ['tryanderror'],
     packages = find_packages(),
-    version = '1.5',
+    include_package_data=True,    # include everything in source control
+    package_data = {'': '*.xlsx'},
+    version = '1.6',
     description = 'pip install postalcode',
     author = 'ekils',
     author_email = 'bobobo746@hotmail.com',
@@ -12,5 +13,4 @@ setup(
     download_url = 'https://github.com/ekils/postalcode.git',
     keywords = ['Good Project'],
     classifiers = [],
-    package_data = {'taiwan': 'zip_code.xlsx'},
 )
