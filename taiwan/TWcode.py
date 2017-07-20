4e9dd504ec65c7a54a1b82b8b511157d3774020c
dannyliu <dannyliu@ixensor.com>
2017-07-21 00:06:28 +0800

    10th commit

--- taiwan/TWcode.py
+++ taiwan/TWcode.py
@@ -6,5 +6,5 @@ def ZipCodeTW(city,district):
     city=unicode(city,'utf-8')
     district= unicode(district,'utf-8')
     r= pd.read_excel(_chp_csv_path,sheetname=u'{}'.format(city))
-    print r[u'{}'.format(district)][0]
+    return r[u'{}'.format(district)][0]
 #ZipCodeTW('新北市','新店區')
